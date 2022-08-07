31d7175ad210a6ed9545c741f24061aece877795
Camilo Santander <camilo.santander1991@gmail.com>
2022-08-07 18:24:59 -0400

    Curso Flutter 47

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:counter_app/screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Center(child: Text('Hola Mundo')));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
